8953201c7a9e73a514cbeaa21d8267420ce23201
Roger Allen <rallen@gmail.com>
2013-08-08 23:25:10 -0700

    add fingers.  bit of code cleanup

--- src/leapfun/f003.clj
+++ src/leapfun/f003.clj
@@ -22,8 +22,18 @@
                      {:key :y     :text "Y"}
                      {:key :z     :text "Z"}
 ]
-           :rows [{:name "hand 0" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
-                  {:name "hand 1" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+           :rows [{:name "hnd0"     :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd0fgr0" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd0fgr1" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd0fgr2" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd0fgr3" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd0fgr4" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd1"     :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd1fgr0" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd1fgr1" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd1fgr2" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd1fgr3" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
+                  {:name "hnd1fgr4" :id "n/a" :valid "n/a" :x "n/a" :y "n/a" :z "n/a"}
                   ]]))
 
 (defn update-table-xyz
@@ -33,40 +43,68 @@
                               :y (format "%5.3f" y)
                               :z (format "%5.3f" z)}))
 
-(defmacro get-hands-fn
-  [hands f]
-  `(for [i# (range (.count ~hands))]
-     (. (.get ~hands i#) ~f)))
+(defmacro map-javalist
+  [list f]
+  `(for [i# (range (.count ~list))]
+     (. (.get ~list i#) ~f)))
 
 (defn get-hands-xyz
   [hands interaction-box]
   (for [i (range (.count hands))]
-    (let [pos (->> (.get hands i)
-                   (.palmPosition)
-                   (.normalizePoint interaction-box))]
+    (let [pos (.normalizePoint interaction-box
+                               (.palmPosition (.get hands i))
+                               false)]
       (vector (.getX pos) (.getY pos) (.getZ pos)))))
+
+(defn get-pointable-xyz
+  [pointable interaction-box]
+  (for [i (range (.count pointable))]
+    (let [pos (.normalizePoint interaction-box
+                               (.tipPosition (.get pointable i))
+                               false)]
+      (vector (.getX pos) (.getY pos) (.getZ pos)))))
+
+(defn update-row-missing
+  [ii]
+  (update-at! @the-table ii {:id "None"})
+  (update-at! @the-table ii {:valid "None"})
+  (update-table-xyz ii [Double/NaN Double/NaN Double/NaN]))
+
+(defn update-row
+  [i ii ids vlds xyzs]
+  (try (let [id (nth ids i)
+             vld (nth vlds i)
+             xyz (nth xyzs i)]
+         (update-at! @the-table ii {:id (str id)})
+         (update-at! @the-table ii {:valid (str vld)})
+         (update-table-xyz ii xyz))
+       (catch IndexOutOfBoundsException e
+         (update-row-missing ii))))
 
 (defn update-table
   [ctlr]
   (let [frame           (.frame ctlr)
         hands           (.hands frame)
         interaction-box (.interactionBox frame)
-        ids             (get-hands-fn hands id)
-        vlds            (get-hands-fn hands isValid)
-        xyzs            (get-hands-xyz hands interaction-box)]
+        hand-ids        (map-javalist hands id)
+        hand-vlds       (map-javalist hands isValid)
+        finger-lists    (map-javalist hands fingers)
+        hand-xyzs       (get-hands-xyz hands interaction-box)]
     (dotimes [i 2]
-      (let [ii (* 1 i)]
-        (try (let [id (nth ids i)
-                   vld (nth vlds i)
-                   xyz (nth xyzs i)]
-               (update-at! @the-table ii {:id (str id)})
-               (update-at! @the-table ii {:valid (str vld)})
-               (update-table-xyz ii xyz))
-             (catch IndexOutOfBoundsException e
-               (do
-                 (update-at! @the-table ii {:id "None"})
-                 (update-at! @the-table ii {:valid "None"})
-                 (update-table-xyz ii [Double/NaN Double/NaN Double/NaN]))))))))
+      (let [ii (* 6 i)]
+        (update-row i ii hand-ids hand-vlds hand-xyzs)
+        (try
+          (let [fingers     (nth finger-lists i)
+                finger-ids  (map-javalist fingers id)
+                finger-vlds (map-javalist fingers isValid)
+                finger-xyzs (get-pointable-xyz fingers interaction-box)]
+            (dotimes [j 5]
+              (let [jj (+ ii j 1)]
+                (update-row j jj finger-ids finger-vlds finger-xyzs))))
+          (catch IndexOutOfBoundsException e
+            (dotimes [j 5]
+              (let [jj (+ ii j)]
+                (update-row-missing jj)))))))))
 
 (defn poll-controller
   []
